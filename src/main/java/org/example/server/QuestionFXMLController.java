ffcb871e307d7c62d23470e1b2b3bcff50277fa8
Shaxboz1 <shaxbozboyhanov1@gmail.com>
2025-01-20 23:03:45 +0500

    qoshdim

--- src/main/java/org/example/server/QuestionFXMLController.java
+++ src/main/java/org/example/server/QuestionFXMLController.java
@@ -20,7 +20,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
+//jbhjkbhjb
 public class QuestionFXMLController {
     @FXML
     private Label savoLabel;
